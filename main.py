e6623b7701831138a5ed01ea3d935e34424a0ca1
amjadcp <amjadcp03@gmail.com>
2022-04-16 21:27:47 +0530

    add:basic info

--- main.py
+++ main.py
@@ -2,37 +2,51 @@ import tweepy
 from keys import *
 
 
-# client = tweepy.Client(bearer_token)
+client = tweepy.Client(bearer_token)
 
-# user = client.get_user(username='Naheel360')
-# user_id = user[0].id
-# print(client.get_users_followers(id=user_id))
+# client = tweepy.Client(
+#     bearer_token=bearer_token,
+#     consumer_key=consumer_key,
+#     consumer_secret=consumer_secret,
+#     access_token=access_token,
+#     access_token_secret=access_token_secret,
+# )
 
 
-client = tweepy.Client(
-    bearer_token=bearer_token,
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    access_token=access_token,
-    access_token_secret=access_token_secret,
-)
+username = input('Enter a tweeter ID : ')
 
-user = client.get_user(username='Naheel360')
+user = client.get_user(username=username)
 user_id = user[0].id
-# print(client.get_users_followers(id=user_id))
 
-# mentions = client.get_users_mentions(id=user_id)
-# for mention in mentions[0]:
-#     print(mention)
+try:
+    followers = len(client.get_users_followers(id=user_id)[0])
+except:
+    followers = 0
+try:
+    followings = len(client.get_users_following(id=user_id)[0])
+except:
+    followings = 0
+try:
+    mentions = client.get_users_mentions(id=user_id)[0]
+except:
+    mentions = 0
+try:
+    tweets = len(client.get_users_tweets(id=user_id)[0])
+except:
+    tweets = 0
+try:
+    liked_tweet = len(client.get_liked_tweets(id=user_id)[0])
+except:
+    liked_tweet = 0
+# get_tweet = client.get_tweet(id=1514564579946541060)
+# print(get_tweet[0].text)
 
-# details of me
-# me = client.get_me()
-# print(me[0].id)
+print('Followers :', followers)
+print('Followings :', followings)
+print('Mentioned in Messages :', mentions)
+print('No. of Tweets :', tweets)
+print('No. of Liked Tweets :',liked_tweet)
 
-#tweets of that user
-# tweets = client.get_users_tweets(id=user_id)
-# print(tweets)
+# quote = client.get_quote_tweets(id=1514564579946541060)
 
-auth = tweepy.OAuth2BearerHandler(bearer_token)
-api = tweepy.API(auth)
-api.retweet(id=1488439159299526657)
+
